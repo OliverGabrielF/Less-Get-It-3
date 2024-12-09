caaa83c3e4ec0a2ed5dfd05d7039af5052887f08
Oliver Gabriel <ofiglarski@gmail.com>
2024-12-09 22:06:21 +0000

    Fix for indentation of code

--- cinema/src/screens/home_screen.js
+++ cinema/src/screens/home_screen.js
@@ -3,13 +3,13 @@ import { Text, View } from 'react-native';
 import styles from '../styles/general_style.js';
 
 export default function HomeScreen({ }) {
-    return (
-        <View>
-            <View style={StyleSheet.container}>
-                <View style={styles.header}>
-                    <Text style={styles.header_text}>Home</Text>
-                </View>
-            </View>
+  return (
+    <View>
+      <View style={StyleSheet.container}>
+        <View style={styles.header}>
+          <Text style={styles.header_text}>Home</Text>
         </View>
-    );
+      </View>
+    </View>
+  );
 }
--- cinema/src/styles/general_style.js
+++ cinema/src/styles/general_style.js
@@ -1,23 +1,23 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#cccccc',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-    },
-    header: {
-        height: 90,
-        backgroundColor: '#555555',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    header_text: {
-        marginTop: 30,
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: 'white',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#cccccc',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  header: {
+    height: 90,
+    backgroundColor: '#555555',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header_text: {
+    marginTop: 30,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
 })
