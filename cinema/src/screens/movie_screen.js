dcc5e2d803a3500bf1d5e429caed98b202f2dd8a
RunicSyntax <107773663+RunicSyntax@users.noreply.github.com>
2024-12-13 23:12:26 +0000

    Update to movie_screen

--- cinema/src/screens/movie_screen.js
+++ cinema/src/screens/movie_screen.js
@@ -1,9 +1,8 @@
 import React, { useEffect } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View, Image } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { fetchMovies } from '../actions/moviesActions.js';
-import { fetchUpcoming } from '../actions/upcomingActions.js';
 import styles from '../styles/movie_screen_style.js';
 
 import Header from '../components/header.js';
@@ -14,30 +13,57 @@ export default function MovieScreen({ navigation, route }) {
 
   const dispatch = useDispatch();
   const { movies } = useSelector((state) => state.movies);
-  const { upcoming } = useSelector((state) => state.upcoming)
 
-  let movie = movies.find((m) => m.id === movieId);
-  if (!movie) {
-    movie = upcoming.find((m) => m.id === movieId);
-  }
+  const movie = movies.find((m) => m.id === movieId);
 
   const { token } = useSelector((state) => state.authentication);
 
   useEffect(() => {
     if (token) {
       dispatch(fetchMovies());
-      dispatch(fetchUpcoming())
     }
   }, [token, dispatch]);
+
+  console.log(movie.plot);
 
   return (
     <View style={styles.container}>
       <Header navigation={navigation}/>
-      <View style={styles.movie_info_container}>
-        <Text>{movie.title}</Text>
-      </View>
+      <View style={styles.background_container}>
+        <View style={styles.flex_row_container}>
+          <View style={styles.poster_container}>
+            <Image source={{ uri: movie.poster }} style={styles.poster} />
+          </View>
+          <View style={styles.movie_details_container}>
+            <Text style={styles.movie_name}>{movie.title}</Text>
 
+            <View style={styles.detail_duration_row}>
+              <Text style={styles.detail_duration_title}>Lengd: </Text>
+              <Text style={styles.detail_duration_info}>{movie.durationMinutes} min.</Text>
+            </View>
+            
+            <View style={styles.detail_year_row}>
+              <Text style={styles.detail_year_title}>Frumsýnd: </Text>
+              <Text style={styles.detail_year_info}>{movie.year}</Text>
+            </View>
+
+            <View style={styles.genre_container}>
+              <Text style={styles.genre_title}>Tegund: </Text>
+              {movie.genres.map((genre, index) => (
+                <Text key={genre.ID} style={styles.genre_text}>
+                {genre.Name}
+                {index < movie.genres.length - 1 ? ', ' : ''}
+                </Text>
+              ))}
+            </View>
+          </View>
+        </View>
+          
+        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Sed interdum faucibus tincidunt. Nunc congue eros eget accumsan hendrerit. 
+        Integer aliquam eros elit, a laoreet dolor tincidunt nec. 
+        Phasellus cursus neque eu aliquam tincidunt. </Text>
+      </View>
     </View>
   );
 }
-
--- cinema/src/styles/movie_screen_style.js
+++ cinema/src/styles/movie_screen_style.js
@@ -7,6 +7,80 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
+  background_container: {
+    flex: 1,
+    width: '95%',
+    marginVertical: 10,
+    padding: 10,
+    borderRadius: 20,
+    backgroundColor: '#DAE2DF',
+  },
+  flex_row_container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  poster_container: {
+    width: '50%',
+    overflow: 'hidden',
+  },
+  poster: {
+    resizeMode: 'cover',
+    aspectRatio: 3/4,
+    borderRadius: 10,
+  },
+  movie_details_container: {
+    width: '50%',
+    padding: 10,
+    backgroundColor: 'blue',
+  },
+  movie_name: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#454B66',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  detail_duration_row: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  detail_duration_title: {
+    fontWeight: 'bold',
+    width: 50,
+    fontSize: 14,
+  },
+  detail_duration_info: {
+    fontWeight: 'normal',
+    fontSize: 14,
+  },
+  detail_year_row: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  detail_year_title: {
+    fontWeight: 'bold',
+    width: 70,
+    fontSize: 14,
+  },
+  detail_year_info: {
+    fontWeight: 'normal',
+    fontSize: 14,
+  },
+  genre_container: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    alignContent: 'flex-start',
+  },
+  genre_title: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    width: 52,
+    marginRight: 3,
+  },
+  genre_text: {
+    fontSize: 14,
+    marginRight: 3,
+  },
 })
 
 export default styles;
