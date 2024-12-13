4140629ef52a1b4ffbda1f8503795adf5cc51960
RunicSyntax <107773663+RunicSyntax@users.noreply.github.com>
2024-12-13 00:27:10 +0000

    Some styling updates

--- cinema/src/styles/upcoming_style.js
+++ cinema/src/styles/upcoming_style.js
@@ -2,17 +2,16 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   pressable: {
-    flex: 1,
     flexBasis: '45%',
     maxWidth: '45%',
     alignItems: 'center',
     justifyContent: 'center',
     overflow: 'hidden',
     marginBottom: 20,
-    marginHorizontal: '1%',
+    marginHorizontal: '2%',
   },
   container: {
-    height: 200,
+    height: 250,
     width: '100%', 
     backgroundColor: '#DAE2DF',
     alignItems: 'center',
@@ -23,26 +22,32 @@ const styles = StyleSheet.create({
     shadowRadius: 8,
     shadowOffset: { width: 0, height: 6 },
     elevation: 10,
-    padding: 10,
+    paddingBottom: 10,
   },
-  name_container: {
-    width: 160,
+  thumbnail_container: {
+    width: '100%',
+    height: '75%',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    overflow: 'hidden',
+  },
+  thumbnail: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
   },
   name: {
+    marginTop: 10,
     fontSize: 16,
     fontWeight: 'bold',
     color: '#555555',
     textAlign: 'center',
   },
-  thumbnail_container: {
-    width: 100,
-    height: 100,
-  },
-  thumbnail: {
-    width: 100,
-    height: 100,
-    borderRadius: 20,
-    aspectRatio: 1,
+  release_date: {
+    fontSize: 14,
+    color: '#555555',
+    textAlign: 'center',
+    marginTop: 2,
   },
 });
 
